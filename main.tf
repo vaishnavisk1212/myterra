6ebf7baf9a29f771f73a5e75ee39a38738efc1e2
vaishnavi <129852316+vaishnavisk1212@users.noreply.github.com>
2025-02-08 16:08:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ terraform {
 }
 provider "aws" {
   region = var.my_region
-  access_key = var.access_key
-  secret_key = var.secret_key
+  #access_key = var.access_key
+  #secret_key = var.secret_key
 }
 resource "aws_instance" "myec2" {
   ami           = var.my_ami
